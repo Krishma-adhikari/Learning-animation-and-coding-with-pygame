3c9fd95c89e90116d407703e14e1f986978f15ae
Krishma Adhikari <adhikarikrishma2@gmail.com>
2026-05-10 13:10:40 +0545

    .

---
+++ Basics/day4.py
@@ -0,0 +1,87 @@
+# Optimization and OOP
+
+
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((500,500))
+clock = pygame.time.Clock()
+pygame.display.set_caption("Using OOP.")
+
+class player(object):
+    def __init__(self,x,y,width,height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.isJump = False
+        self.left = False
+        self.right = False
+        self.walkCount = 0
+        self.jumpCount = 10
+
+    def draw(self,screen):
+        if self.walkCount+1>=27:
+            self.walkCount = 0
+        if self.left:
+           screen.blit(walkLeft[self.walkCount//3],(self.x,self.y))
+           self.walkCount += 1        
+        elif per.right:
+          screen.blit(walkRight[self.walkCount//3],(self.x,self.y))
+          self.walkCount += 1  
+        else:
+          screen.blit(char,(self.x,self.y))   
+ 
+
+walkRight = [pygame.image.load('R1.png'),pygame.image.load('R2.png'),pygame.image.load('R3.png'),pygame.image.load('R4.png'),pygame.image.load('R5.png'),pygame.image.load('R6.png'),pygame.image.load('R7.png'),pygame.image.load('R8.png'),pygame.image.load('R9.png')]
+walkLeft = [pygame.image.load('L1.png'),pygame.image.load('L2.png'),pygame.image.load('L3.png'),pygame.image.load('L4.png'),pygame.image.load('L5.png'),pygame.image.load('L6.png'),pygame.image.load('L7.png'),pygame.image.load('L8.png'),pygame.image.load('L9.png')]
+bg = pygame.image.load('bg.jpg')
+char = pygame.image.load('standing.png')
+
+run = True
+
+
+def redrawGamewindow():
+    screen.blit(bg,(0,0))
+    per.draw(screen)
+    pygame.display.update()
+
+per = player(280,380,64,64)
+while run:
+    clock.tick(50)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT ] and per.x <500-per.width-per.vel:
+        per.x+=per.vel   
+        per.left = False
+        per.right = True
+    elif keys[pygame.K_LEFT] and per.x > per.vel:
+        per.x-=per.vel
+        per.left = True
+        per.right = False
+    else:
+        per.left = False
+        per.right = False
+        per.walkCount = 0
+    if not(per.isJump):
+        
+        if keys[pygame.K_SPACE]:
+            per.isJump = True
+            per.right = False
+            per.left = False
+            per.walkCount = 0
+    else:
+        if per.jumpCount>=-10:
+            neg = 1
+            if(per.jumpCount<0):
+                neg = -1
+            per.y-=(per.jumpCount ** 2)*0.5*neg
+            per.jumpCount-=1
+        else:
+            per.isJump = False
+            per.jumpCount = 10
+    redrawGamewindow()
+pygame.quit
